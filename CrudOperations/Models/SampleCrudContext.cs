296c2802636e1f08c98ce6271ec9ba6d0c6b30f1
Rahul <81146148+rah-k@users.noreply.github.com>
2023-01-07 12:40:35 +0530

    Add files via upload

---
+++ CrudOperations/Models/SampleCrudContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CurdOperation.Models
+{
+    public class SampleCrudContext:DbContext
+    {
+        public SampleCrudContext(DbContextOptions<SampleCrudContext>options):base(options)
+        {
+
+        }
+        public  DbSet<StudentModel> StudentData { get; set; }
+    }
+}
---
+++ CrudOperations/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CurdOperation.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+        public string Standard { get; set; }
+        public int Age { get; set; }
+    }
+}
